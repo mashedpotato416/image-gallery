212601e2dd4b03bd638194092158bed619d0bc4e
Hector Vergara <hector.vergara416@gmail.com>
2020-11-11 19:02:06 +0800

    feat: add change image feature
    Added a feature that when user clicks on a thumbnail, the display image
    will change to what the thumbnail is.

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,31 @@
+function ChangeDisplayImage(e) {
+  var imagePath = e.target.src
+  displayedImage.src = imagePath
+}
+
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Looping through images */
+// Looping through images
+const numberOfImages = 5
+var count = 1 
+while( count <= numberOfImages ) {
+  
+  var imgName = 'pic' + count
+  var filePath = 'images/' + imgName + '.jpg'
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', filePath);
+  newImage.setAttribute('class', imgName);
+  thumbBar.appendChild(newImage);
 
-/* Wiring up the Darken/Lighten button */
+  newImage.addEventListener('click', ChangeDisplayImage)
+
+  count++
+}
+  
+
+// Wiring up the Darken/Lighten button
