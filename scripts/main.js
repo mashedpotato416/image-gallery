3b272549cdb7ebfb1c392aa1914b4360b88b094a
Hector Vergara <hector.vergara416@gmail.com>
2020-11-11 19:24:30 +0800

    feat: add scripts for light/darken feature
    Added script to change the alpha of the overlay.

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,27 @@ function ChangeDisplayImage(e) {
   displayedImage.src = imagePath
 }
 
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+function ChangeButton(e) {
+  var currentButton = e.target.textContent
+  switch (currentButton) {
+    case 'Darken':
+      newContent = 'Lighten'
+      newAlpha = 0.5
+      break;
+    case 'Lighten':
+      newContent = 'Darken'
+      newAlpha = 0.
+      break;
+    default:
+      newContent = "Error"
+      newAlpha = 1.0
+  }
+  e.target.textContent = newContent
+  overlay.style.backgroundColor = 'rgba(0,0,0,' + newAlpha + ')'
+}
+
+const displayedImage = document.querySelector('.displayed-img')
+const thumbBar = document.querySelector('.thumb-bar')
 
 // Looping through images
 const numberOfImages = 5
@@ -14,10 +33,10 @@ while( count <= numberOfImages ) {
   var imgName = 'pic' + count
   var filePath = 'images/' + imgName + '.jpg'
 
-  const newImage = document.createElement('img');
-  newImage.setAttribute('src', filePath);
-  newImage.setAttribute('class', imgName);
-  thumbBar.appendChild(newImage);
+  const newImage = document.createElement('img')
+  newImage.setAttribute('src', filePath)
+  newImage.setAttribute('class', imgName)
+  thumbBar.appendChild(newImage)
 
   newImage.addEventListener('click', ChangeDisplayImage)
 
@@ -26,5 +45,7 @@ while( count <= numberOfImages ) {
 
 // Wiring up the Darken/Lighten button
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('button')
+const overlay = document.querySelector('.overlay')
+
+btn.addEventListener('click', ChangeButton )
