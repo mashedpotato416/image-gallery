e3efea0247028044d4322fac55607e14bc27ea89
Hector Vergara <hector.vergara416@gmail.com>
2020-11-11 19:34:12 +0800

    docs: add comments to codes for documentation

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
+// Change the display image based on clicked thumbnail
 function ChangeDisplayImage(e) {
   var imagePath = e.target.src
   displayedImage.src = imagePath
 }
 
+// Change text of the button and alpha(rgba) of the overlay
 function ChangeButton(e) {
   var currentButton = e.target.textContent
   switch (currentButton) {
@@ -22,30 +24,34 @@ function ChangeButton(e) {
   overlay.style.backgroundColor = 'rgba(0,0,0,' + newAlpha + ')'
 }
 
+// Loop through images in directory
+
 const displayedImage = document.querySelector('.displayed-img')
 const thumbBar = document.querySelector('.thumb-bar')
-
-// Looping through images
 const numberOfImages = 5
-var count = 1 
+var count = 1 // Initialize
 while( count <= numberOfImages ) {
   
+  // Create containers for image name and file path
   var imgName = 'pic' + count
   var filePath = 'images/' + imgName + '.jpg'
 
+  // Create html img element
   const newImage = document.createElement('img')
   newImage.setAttribute('src', filePath)
   newImage.setAttribute('class', imgName)
   thumbBar.appendChild(newImage)
 
+  // Event listener for thumbnails
   newImage.addEventListener('click', ChangeDisplayImage)
 
-  count++
+  count++ // Final Expression
 }
 
-// Wiring up the Darken/Lighten button
+// Darken/Lighten button
 
-const btn = document.querySelector('button')
+const btn = document.querySelector('button') 
 const overlay = document.querySelector('.overlay')
 
+// Event listener for button
 btn.addEventListener('click', ChangeButton )
